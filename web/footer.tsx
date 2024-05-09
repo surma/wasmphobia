e848fde20ead40866a44ca39433b5e4e5c36510d
Surma <surma@surma.dev>
2024-05-09 12:50:38 +0100

    Reapply "Show commit hash on page"
    This reverts commit f526510c2fd504967950f8bd84eedd4f7c8928b1.

--- web/footer.tsx
+++ web/footer.tsx
@@ -1,12 +1,13 @@
 import { Box, Link } from "@primer/react";
 import * as React from "react";
+import CommitHash from "./commit-hash";
 
 export default function Footer() {
   return (
     <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
       <Box sx={{ textAlign: "center" }}>
         Made with 🤦‍♂️ by <Link href="https://x.com/dassurma" target="_blank">Surma</Link>. Source code on{" "}
-        <Link href="https://github.com/surma/wasmphobia" target="_blank">GitHub</Link>.
+        <Link href="https://github.com/surma/wasmphobia" target="_blank">GitHub</Link>. Commit <CommitHash length={6} />
       </Box>
     </Box>
   );
